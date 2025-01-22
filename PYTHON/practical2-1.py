6ed2a09ec52e6823015c55c768b9e2e39b202074
Omkar khaire <khaireomkar12@gmail.com>
2025-01-22 20:11:11 +0530

    Delete PYTHON directory

--- PYTHON/practical2-1.py
+++
@@ -1,3 +0,0 @@
-# 1. Write python program display welcome message on Screen.
-
-print("Hello word")
--- PYTHON/practical2-2.py
+++
@@ -1,4 +0,0 @@
-# 2. implement the python program to read data from user and display data on screen.
-
-name = input("Enter Name of student : ")
-print("Student name is : ", name)
